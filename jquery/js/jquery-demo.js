debf9ae9a36326a77a9fc0c8f3f1a0abffbad26d
David Stearns <dlsinfo@uw.edu>
2013-10-23 20:13:06 -0700

    stubs for jQuery demo

---
+++ jquery/js/jquery-demo.js
@@ -0,0 +1,12 @@
+/* jquery-demo.js
+    script for lecture on jQuery library
+*/
+
+var names = [
+    {first: 'Thorfin', last: 'Skullspitter'},
+    {first: 'Amy', last: 'Chen'},
+    {first: 'Dominique', last: 'Jones'},
+    {first: 'Andrew', last: 'Matsui'},
+    {first: 'George', last: 'Brown'}
+];
+
