26043d19b0bd02b3e3bc003ee4914603b3e5058b
unknown <iguest@is-m430m27-01.ischool.uw.edu>
2013-10-29 12:22:44 -0700

    end of class code

--- forms/js/forms-demo.js
+++ forms/js/forms-demo.js
@@ -2,3 +2,32 @@
 // script for in-class HTML forms demo
 //
 
+//doc ready function
+$(function(){
+	var select = $('select[name="state"]');
+	var option;
+	var idx;
+	var state;
+
+	for(idx = 0; idx < states.length; ++idx) {
+		state = states[idx];
+		option = $(document.createElement('option'));
+		option.attr('value', state.abbreviation);
+		option.html(state.name);
+		select.append(option);
+	}
+
+	$('.signup-form').submit(function(){
+		var nameInput = $('.signup-form input[name="name"]');
+		var value = nameInput.val();
+		if (value && value.trim().length > 0)
+			return true;
+		else {
+			alert("Only spaces is not allowed!");
+			return false;			
+		}
+	});
+
+
+
+});
