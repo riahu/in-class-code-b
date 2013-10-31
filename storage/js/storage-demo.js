4df88bc5c166e5617f1f76df3b4e461600385c92
unknown <iguest@is-m430m27-01.ischool.uw.edu>
2013-10-31 11:19:36 -0700

    code from first half of class

--- storage/js/storage-demo.js
+++ storage/js/storage-demo.js
@@ -4,3 +4,27 @@
 
 const BG_COLOR_KEY = 'bgcolor';
 
+//doc ready function
+$(function(){
+
+	if (Modernizr.localstorage) {
+		var bgColor = localStorage.getItem(BG_COLOR_KEY);
+		$('body').css('background-color', bgColor);
+		$('.bg-color-input').val(bgColor);
+	}
+
+	$('.save-bg-color').click(function(){
+		if (Modernizr.localstorage) {
+			var bgColor = $('.bg-color-input').val();
+			localStorage.setItem(BG_COLOR_KEY, bgColor);
+			$('body').css('background-color', bgColor);
+		}
+	});
+
+	$('.clear-bg-color').click(function(){
+		if (Modernizr.localstorage) {
+			localStorage.removeItem(BG_COLOR_KEY);
+			window.location.reload();
+		}
+	});
+});
