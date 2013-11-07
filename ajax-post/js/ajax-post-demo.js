753a95af057870015bd8f1394c5a907661dd3705
Dave Stearns <dlsinfo@uw.edu>
2013-11-07 07:58:06 -0800

    add htmlEncode function

--- ajax-post/js/ajax-post-demo.js
+++ ajax-post/js/ajax-post-demo.js
@@ -4,6 +4,23 @@
     URL for posting: https://courses.washington.edu/info343/ajax/sayhello/
 */
 
+//htmlEncode()
+// encodes the passed string of HTML so that it can
+// be safely added to a page without being interpreted
+// as HTML markup (with potentially harmful effects)
+// source: http://stackoverflow.com/questions/1219860/html-encoding-in-javascript-jquery
+// parameters:
+//  - s (string) value to html-encode
+//  - return value (string) encoded HTML value
+//
+function htmlEncode(s) {
+    //create an in-memory div element
+    var div = document.createElement('div');
+    //append the string to encode as a text node
+    div.appendChild(document.createTextNode(s));
+    //return the innerHTML property (which will be encoded)
+    return div.innerHTML;
+} //htmlEncode()
 
 // function to display any ajax errors we get
 $(document).ajaxError(function(event, jqXHR, err){
