ab84052c4251a5e75c733acc8e97c05fd2ba7ae3
unknown <iguest@is-m430m27-01.ischool.uw.edu>
2013-11-07 12:08:58 -0800

    end of mashup demo code

--- mashup/js/mashup-demo.js
+++ mashup/js/mashup-demo.js
@@ -6,3 +6,36 @@
     UW Lat/Lng is 47.6559/-122.3031
 */
 
+$(function(){
+	$.getJSON('http://data.seattle.gov/resource/kzjm-xkqj.json', plotIncidents);
+}); //on doc ready
+
+function plotIncidents(incidents) {
+	var idx;		//loop counter
+	var incident;	//current incident
+	var map;		//google map
+	var marker;		//google map marker
+
+	map = new google.maps.Map($('.map-container')[0], {
+		center: new google.maps.LatLng(47.6559, -122.3031),
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		streetViewControl: false
+	});
+
+	for (idx = 0; idx < incidents.length; ++idx) {
+		incident = incidents[idx];
+
+		if (incident.latitude && incident.longitude) {
+			marker = new google.maps.Marker({
+				map: map,
+				position: new google.maps.LatLng(incident.latitude, incident.longitude),
+				title: incident.type
+			});
+		} //if lat/lng
+	} //for each incident
+
+	$('.last-update').html(new Date().toLocaleString());
+
+
+} //plotIncidents()
